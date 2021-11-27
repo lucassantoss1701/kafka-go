2dd8208e91572873779ac90b938fd8f0a8786944
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-11-26 22:51:46 -0300

    kafka with go

---
+++ cmd/producer/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	producer := NewKafkaProducer()
+	Publish("mensagem", "teste1", producer, nil)
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "gokafka_kafka_1:9092",
+	}
+
+	producer, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	return producer
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
